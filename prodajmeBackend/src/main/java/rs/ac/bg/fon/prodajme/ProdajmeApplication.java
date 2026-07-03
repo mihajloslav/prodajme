48a504e189b728ee0f7de9c30fa8c89bd80a657f
mihajloslav <95142179+mihajloslav@users.noreply.github.com>
2026-07-04 01:52:09 +0200

    initial commit

---
+++ prodajmeBackend/src/main/java/rs/ac/bg/fon/prodajme/ProdajmeApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.prodajme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdajmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdajmeApplication.class, args);
+	}
+
+}
