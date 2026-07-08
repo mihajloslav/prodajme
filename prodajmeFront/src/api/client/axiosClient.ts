3455fc3970b66180091c488c76efd259347086c0
mihajloslav <95142179+mihajloslav@users.noreply.github.com>
2026-07-08 13:07:39 +0200

    a

--- prodajmeFront/src/api/client/axiosClient.ts
+++ prodajmeFront/src/api/client/axiosClient.ts
@@ -1,17 +1,21 @@
 import axios from 'axios'
 
+// Zajednička Axios instanca za komunikaciju sa backendom
 const axiosClient = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
 })
 
+// OVO PRE svakog zahteva dodaje JWT token ukoliko korisnik ima aktivnu sesiju
 axiosClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('prodajme_jwt_token')
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
+
     return config
   },
   (error) => {
@@ -22,12 +26,15 @@ axiosClient.interceptors.request.use(
 axiosClient.interceptors.response.use(
   (response) => response,
   (error) => {
+
     // Ako backend vrati 401, korisnik se automatski odjavljuje
     if (error.response && error.response.status === 401) {
       localStorage.removeItem('prodajme_current_user')
       localStorage.removeItem('prodajme_jwt_token')
+
       window.location.href = '/login'
     }
+
     return Promise.reject(error)
   }
 )
